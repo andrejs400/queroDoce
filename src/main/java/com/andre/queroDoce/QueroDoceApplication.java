c69edd28cbd937b1eabc716694c6743f52521da8
andrejs400 <andrejs400@gmail.com>
2022-09-12 17:46:23 -0300

    first commit

---
+++ src/main/java/com/andre/queroDoce/QueroDoceApplication.java
@@ -0,0 +1,13 @@
+package com.andre.queroDoce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueroDoceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueroDoceApplication.class, args);
+	}
+
+}
